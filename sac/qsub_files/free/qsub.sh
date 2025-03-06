6f91e2ad160ab9600e5b4125b841793c906f4adb
gabes135 <gabes135@gmail.com>
2025-03-06 14:12:23 -0500

    message

---
+++ sac/qsub_files/free/qsub.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#$ -l h_rt=11:59:99
+#$ -j y
+#$ -o jobs/out.txt
+#$ -N edge
+
+mkdir -p jobs # creates folder for qsub out files
+mkdir -p out_files # creates folder for SAC out files
+
+echo "Start $JOB_NAME-$JOB_ID: $(date)"
+
+module load julia
+
+julia sac_freee.jl
+
+echo "End $JOB_NAME-$JOB_ID: $(date) "
