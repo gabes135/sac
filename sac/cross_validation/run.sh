12c5125bec5e9293d65de818591bce01674d3737
gabes135 <gabes135@gmail.com>
2025-04-16 12:58:17 -0400

    message

--- sac/cross_validation/run.sh
+++ sac/cross_validation/run.sh
@@ -3,7 +3,7 @@ reps=$1
 cd process_G 
 for r in $(seq 1 $reps);
 do
-    julia make_tin_bosonic.jl ../in_files/hchain_beta2048 $r
+    julia make_tin_cv.jl ../in_files/hchain_beta2048 $r
 
     julia cross_val.jl free 1
 done
